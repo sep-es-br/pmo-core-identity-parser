ab5d3a407e2ad4ca9522b907e11248d092ffa130
geanCarneiro <geancarneiro1@gmail.com>
2026-04-20 10:43:20 -0300

    ajustes

--- src/test/java/identity/parser/pmo_base/PmoBaseApplicationTests.java
+++ src/test/java/identity/parser/pmo_base/PmoBaseApplicationTests.java
@@ -1,13 +1,19 @@
 package identity.parser.pmo_base;
 
+import br.gov.es.pmo.identity.parser.pmo_base.PmoBaseApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 
-@SpringBootTest
+@SpringBootTest(classes = PmoBaseApplication.class)
 class PmoBaseApplicationTests {
+    
+    @MockBean
+    private OAuth2AuthorizedClientService OAuth2AuthorizedClientService;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
