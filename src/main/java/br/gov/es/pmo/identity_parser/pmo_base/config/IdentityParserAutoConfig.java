3d677d350f943ad553cc7468b963b7e99585fffa
geanCarneiro <geancarneiro1@gmail.com>
2026-04-20 15:43:31 -0300

    add autoconfig

---
+++ src/main/java/br/gov/es/pmo/identity_parser/pmo_base/config/IdentityParserAutoConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.gov.es.pmo.identity_parser.pmo_base.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author gean.carneiro
+ */
+@Configuration
+@ComponentScan("br.gov.es.pmo.identity_parser")
+public class IdentityParserAutoConfig {
+    
+}
